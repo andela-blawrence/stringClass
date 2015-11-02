4866915603ca9f968595f7b2e555c9ea7160efe5
andela-blawrence <lawrence.olaiya@andela.com>
2015-11-02 17:28:20 +0100

    add(src) : Code for adding methods to the String Class prototype

--- src/stringClass.js
+++ src/stringClass.js
@@ -1,1 +1,61 @@
 'use strict';
+
+/**
+ * Author : Lawrence Bolutife
+ *
+ * String Class
+ */
+
+/**
+ * [hasVowels description]
+ * @return {Boolean} [description]
+ */
+String.prototype.hasVowels = function () {};
+
+/**
+ * [toUpper description]
+ * @return {String} [description]
+ */
+String.prototype.toUpper = function () {};
+
+/**
+ * [toLower description]
+ * @return {String} [description]
+ */
+String.prototype.toLower = function () {};
+
+/**
+ * [ucFirst description]
+ * @return {String} [description]
+ */
+String.prototype.ucFirst = function () {};
+
+/**
+ * [isQuestion description]
+ * @return {Boolean} [description]
+ */
+String.prototype.isQuestion = function () {};
+
+/**
+ * [words description]
+ * @return {[type]} [description]
+ */
+String.prototype.words = function () {};
+
+/**
+ * [wordCount description]
+ * @return {Number} [description]
+ */
+String.prototype.wordCount = function () {};
+
+/**
+ * [toCurrency description]
+ * @return {String} [description]
+ */
+String.prototype.toCurrency = function () {};
+
+/**
+ * [fromCurrency description]
+ * @return {Number} [description]
+ */
+String.prototype.fromCurrency = function () {};
